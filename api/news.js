d278dc9f15eda9126b5674b6058c711fba10f2cd
dev-sujitdas <dev.sujitdas@gmail.com>
2025-07-21 10:55:32 +0530

    news

--- api/news.js
+++ api/news.js
@@ -1,8 +1,11 @@
+// pages/api/news.js (or /api/news/index.js)
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+const redis = Redis.fromEnv(); // or directly using Redis({...})
 
-export default handler = async (req, res) => {
+const api = `http://api.mediastack.com/v1/news?access_key=d8f89db2a329fc7f8de47b8b6bec02e6&countries=za`;
+
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -11,23 +14,23 @@ export default handler = async (req, res) => {
     return res.status(200).end();
   }
 
-  const cached = await redis.get("mediastack:top");
-  if (cached) {
-    return res.status(200).json(JSON.parse(cached));
-  }
-
   try {
-    const apiKey = process.env.Mediastack_API_Key;
-    const api = `https://api.mediastack.com/v1/news?access_key=${apiKey}&countries=za`;
+    const cached = await redis.get("mediastack:top");
+
+    if (cached) {
+      return res.status(200).json({ data: JSON.parse(cached) });
+    }
 
     const response = await fetch(api);
-    const data = await response.json();
+    const result = await response.json();
 
-    await redis.set("mediastack:top", JSON.stringify(data), { ex: 900 });
+    await redis.set("mediastack:top", JSON.stringify(result.data), {
+      ex: 1200, // 20 minutes
+    });
 
-    return res.status(200).json(JSON.parse(cached));
-
+    return res.status(200).json({ data: result.data });
   } catch (error) {
-    return res.status(500).json({ error: "Failed to fetch news from API" });
+    console.error("API error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
-};
+}
