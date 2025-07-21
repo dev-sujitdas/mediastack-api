948e7d3c28bec089dd7fa61df6eb31b681db8abd
dev-sujitdas <dev.sujitdas@gmail.com>
2025-07-21 09:29:22 +0530

    first commit

---
+++ api/news.js
@@ -0,0 +1,32 @@
+import {Redis} from "@upstash/redis";
+
+const redis = Redis.fromEnv();
+
+export default  handler = async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if(req.method === "OPTIONS"){
+        return res.status(200).end();
+    }
+
+    const cached = await redis.get("mediastack:top");
+    if(cached) {
+        return res.status(200).json(cached);
+    }
+
+    try{
+        const apiKey = process.env.Mediastack_API_Key;
+        const api = `https://api.mediastack.com/v1/news?access_key=${apiKey}&countries=za`;
+
+        const response = await fetch(api);
+        const data = await response.json();
+
+        await redis.set("mediastack:top", data, {ex:900});
+
+        return res.status(200).json(data);
+    }catch(error){
+        return res.status(500).json({error: "Failed to fetch news from API"});
+    }
+}
